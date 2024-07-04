c056ec0af19235ae05ff5d8f9828a39f0adf33ce
naveen <naveennidumolu@gmail.com>
2024-07-04 21:07:05 +0530

    every one point of view is not same

---
+++ src/kalyan_Sir/Be_Kind_To_Every_Person.java
@@ -0,0 +1,11 @@
+package kalyan_Sir;
+
+public class Be_Kind_To_Every_Person
+{
+	public static void main (String [] args)
+	{
+		System.out.println("the fruits of patinence is sweet");
+		
+	}
+
+}
