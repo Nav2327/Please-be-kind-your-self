d38772ccfbee21f47daf856c982aeeb47d89f31e
Nav2327 <158798323+Nav2327@users.noreply.github.com>
2024-07-04 20:58:14 +0530

    Update Be_Friendly_Person.java

--- src/kalyan_Sir/Be_Friendly_Person.java
+++ src/kalyan_Sir/Be_Friendly_Person.java
@@ -6,6 +6,7 @@ public class Be_Friendly_Person
 	{
 		System.out.println("our main agenda is to teach core funcnality to studets like as like as primary colors");
 		System.out.println("Arif is a sadist person");
+		System.out.println("Arif is a good person ");
 	}
 
 }
