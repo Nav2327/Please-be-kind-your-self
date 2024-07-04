b3445741cb7a4d54a180c9ff6c158e80558f1150
naveen <naveennidumolu@gmail.com>
2024-07-04 20:45:00 +0530

    no one can take care of you

---
+++ src/kalyan_Sir/Be_Friendly_Person.java
@@ -0,0 +1,11 @@
+package kalyan_Sir;
+
+public class Be_Friendly_Person
+{
+	public static void main (String [] args)
+	{
+		System.out.println("our main agenda is to teach core funcnality to studets like as like as primary colors");
+		System.out.println("Arif is a sadist person");
+	}
+
+}
